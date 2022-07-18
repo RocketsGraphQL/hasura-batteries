38bc5cda4e46674889d8551ce4bedfcfe11a8259
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2022-07-18 21:31:30 +0530

    Feature/google login (#15)
    * google and facebook logins
    
    * callback URLs
    
    * add fields
    
    * add dev env to gitignore

--- routes/routes.go
+++ routes/routes.go
@@ -291,10 +291,10 @@ type JWTData struct {
 }
 
 type ClientCredentials struct {
-	ClientId         string
-	ClientSecret     string
-	RedirectURL      string
-	FinalRedirectURL string
+	ClientId     string
+	ClientSecret string
+	RedirectURL  string
+	CallbackURL  string
 }
 
 type GithubCredentialsUpdatedResponse struct {
@@ -461,16 +461,23 @@ func getUserDataFromGoogle(code string) ([]byte, error) {
 	return contents, nil
 }
 
+type GoogleUserDetails struct {
+	ID    string
+	Name  string
+	Email string
+}
+
 func ChiGoogleCallback(w http.ResponseWriter, r *http.Request) {
 	log.Println("Here is Google callback!!")
+	var googleUserDetails = &GoogleUserDetails{}
 	// Read oauthState from Cookie
-	oauthState, _ := r.Cookie("oauthstate")
+	// oauthState, _ := r.Cookie("oauthstate")
 
-	if r.FormValue("state") != oauthState.Value {
-		log.Println("invalid oauth google state")
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
+	// if r.FormValue("state") != oauthState.Value {
+	// 	log.Println("invalid oauth google state")
+	// 	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+	// 	return
+	// }
 
 	data, err := getUserDataFromGoogle(r.FormValue("code"))
 	if err != nil {
@@ -482,7 +489,54 @@ func ChiGoogleCallback(w http.ResponseWriter, r *http.Request) {
 	// GetOrCreate User in your db.
 	// Redirect or response with a token.
 	// More code .....
-	fmt.Fprintf(w, "UserInfo: %s\n", data)
+	// fmt.Fprintf(w, "UserInfo: %s\n", data)
+
+	err = json.Unmarshal(data, googleUserDetails)
+	// Add user via passwordless login
+	var provider types.Provider = types.GOOGLE
+	newUser := &AuthService.User{
+		Email: string(googleUserDetails.Email),
+	}
+	dbUser, err := AuthService.PasswordlessProviderLogin(provider, newUser)
+	if err != nil {
+		log.Println("Could not login the user on github")
+		ErrInvalidRequest(err)
+	}
+
+	// generate tokens for the user as usual
+	createdUser := &User{
+		ID:    dbUser.ID,
+		Email: dbUser.Email,
+	}
+	accessToken, refresh := getTokens(createdUser, r)
+	accessTokenCookie := http.Cookie{
+		Name:     "jwt",
+		Value:    accessToken,
+		Expires:  time.Now().Add(20 * time.Minute),
+		SameSite: http.SameSiteNoneMode,
+		// Not setting this will lead to the cookie
+		// being set to the path /api/github
+		// we want the cookie to be accessible by the whole backend
+		Path:   "/",
+		Secure: true,
+	}
+	refreshTokenCookie := http.Cookie{
+		Name:     "refresh",
+		Value:    refresh,
+		Expires:  time.Now().Add(365 * 24 * time.Hour),
+		SameSite: http.SameSiteNoneMode,
+		// Not setting this will lead to the cookie
+		// being set to the path /api/github
+		// we want the cookie to be accessible by the whole backend
+		Path:   "/",
+		Secure: true,
+	}
+	http.SetCookie(w, &accessTokenCookie)
+	http.SetCookie(w, &refreshTokenCookie)
+
+	redirectUrl := os.Getenv("GOOGLE_REDIRECT_URL") + "?rgraphRefreshToken=" + refreshTokenCookie.Value + "&rgraphAccessToken=" + accessTokenCookie.Value
+	// render.Status(r, http.StatusCreated)
+	http.Redirect(w, r, redirectUrl, http.StatusMovedPermanently)
 }
 
 // FacebookUserDetails is struct used for user details
@@ -588,7 +642,8 @@ func ChiFacebookCallback(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &accessTokenCookie)
 	http.SetCookie(w, &refreshTokenCookie)
 
-	redirectUrI := os.Getenv("FACEBOOK_REDIRECT_FINAL_URL") + "?rgraphRefreshToken=" + refreshTokenCookie.Value + "&rgraphAccessToken=" + accessTokenCookie.Value
+	redirectUrI := os.Getenv("FACEBOOK_REDIRECT_URL") + "?rgraphRefreshToken=" + refreshTokenCookie.Value + "&rgraphAccessToken=" + accessTokenCookie.Value
+
 	render.Status(r, http.StatusCreated)
 	http.Redirect(w, r, redirectUrI, http.StatusMovedPermanently)
 	return
@@ -624,9 +679,10 @@ func ChiGoogleSecretsSet(w http.ResponseWriter, r *http.Request) {
 	// Set credentials to the env
 	// I think this is the most appropriate
 	// way to store client ids
-	os.Setenv("GITHUB_CLIENT_ID", data.ClientId)
-	os.Setenv("GITHUB_CLIENT_SECRET", data.ClientSecret)
-	os.Setenv("GITHUB_REDIRECT_URL", data.RedirectURL)
+	os.Setenv("GOOGLE_CLIENT_ID", data.ClientId)
+	os.Setenv("GOOGLE_CLIENT_SECRET", data.ClientSecret)
+	os.Setenv("GOOGLE_CALLBACK_URL", data.CallbackURL)
+	os.Setenv("GOOGLE_REDIRECT_URL", data.RedirectURL)
 
 	render.Status(r, http.StatusCreated)
 	render.Render(w, r, GetGithubCredentialsUpdatedResponse())
@@ -645,8 +701,9 @@ func ChiFacebookSecretsSet(w http.ResponseWriter, r *http.Request) {
 	// way to store client ids
 	os.Setenv("FACEBOOK_APP_ID", data.ClientId)
 	os.Setenv("FACEBOOK_APP_SECRET", data.ClientSecret)
-	os.Setenv("FACEBOOK_REDIRECT_URI", data.RedirectURL)
-	os.Setenv("FACEBOOK_REDIRECT_FINAL_URL", data.FinalRedirectURL)
+
+	os.Setenv("FACEBOOK_CALLBACK_URL", data.CallbackURL)
+	os.Setenv("FACEBOOK_REDIRECT_URL", data.RedirectURL)
 
 	render.Status(r, http.StatusCreated)
 	render.Render(w, r, GetGithubCredentialsUpdatedResponse())
@@ -819,7 +876,8 @@ func generateStateOauthCookie(w http.ResponseWriter) string {
 }
 
 func ChiGoogleClient(w http.ResponseWriter, r *http.Request) {
-	googleRedirectUrl := os.Getenv("GOOGLE_REDIRECT_URL")
+	googleRedirectUrl := os.Getenv("GOOGLE_CALLBACK_URL")
+
 	// Create oauthState cookie
 	oauthState := generateStateOauthCookie(w)
 	// Scopes: OAuth 2.0 scopes provide a way to limit the amount of access that is granted to an access token.
@@ -838,11 +896,13 @@ func ChiGoogleClient(w http.ResponseWriter, r *http.Request) {
 }
 
 func ChiFacebookClient(w http.ResponseWriter, r *http.Request) {
-	githubRedirectUrl := os.Getenv("GITHUB_REDIRECT_URL")
-	providerUrl := "https://www.facebook.com/v14.0/dialog/oauth?client_id=856950301942789&redirect_uri=http://localhost:7000/api/facebook/callback&state={'{st=state123abc,ds=123456789}'}"
-	log.Println("github details: ", githubRedirectUrl, providerUrl)
+	clientId := os.Getenv("FACEBOOK_CLIENT_ID")
+	redirectUrl := os.Getenv("FACEBOOK_CALLBACK_URL")
+	providerUrl := "https://www.facebook.com/v14.0/dialog/oauth?client_id=" + clientId + "&redirect_uri=" + redirectUrl + "&state={'{st=state123abc,ds=123456789}'}"
+	log.Println("github details: ", redirectUrl, providerUrl)
 
-	render.Render(w, r, ClientDetails(githubRedirectUrl, providerUrl))
+	render.Render(w, r, ClientDetails(redirectUrl, providerUrl))
+
 }
 
 // http://localhost:7000/api/facebook/callback?code=AQCD4HmlqBknSSKqqu4jeaKl7ZfSXg9lMkiLIezkMVFpE8jiLhZ296RlKE2WGCBq0cvSmu8sTVJquPY53WkKgJOViH2VKKjlNBu71VWQyMR1cTpQnY5bZ8377yZgAqlTPMiPEiNX5oicpvnArk8iRAAl8TebU_Qp7OyKCgcOq16a-bW1Uys5pjV7JB2rmouHm1EFiMFMy8B3wt5lhBBGJEcX4FBJ72P_fJS4J-izIEgWt_LXcSaOdOcCrxxiytlyqVDL9WIeyzrGW__NDbNy3P6w7b0gB0qFlGgUDr6CN5hwsoxP40ZbUsWdJzMc_eChm7EdP_DX9_3v0ZMCZ4GVYZsS5QUr8Txy18D_S0JqTL8OH-K9w2pn-im67dOUOySInhc#_=_
